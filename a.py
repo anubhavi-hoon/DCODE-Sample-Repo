53e49275fbb8e11b1cd1c2f266d1886057161768
anubhavi-hoon <anshjaiswal127@gmail.com>
2025-09-22 08:45:42 +0530

    Bug Resolved

---
+++ a.py
@@ -0,0 +1,27 @@
+# a = int(input("Enter your first number"))
+# b = int(input("Enter your second number"))
+# print("Select your operators + - * / % **")
+
+# c = (input("Enter operator Symbol"))
+# if c == "*":
+#     print(a*b)
+# elif c == "+":
+#     print(a+b)
+# elif c == "-":
+#     print(a-b)
+# elif c == "/":
+#     print(a/b)
+# elif c =="%":
+#     print(a%b)
+# elif c == "**":
+#     print(a**b)
+
+print(print("hello"))
+
+
+
+
+
+
+
+
---
+++ python.py
@@ -0,0 +1,58 @@
+# def calc_sum(a,b): #function definition 
+#     sum = a+b
+#     print(sum)
+#     return  sum
+# calc_sum(6,9) #function call
+        
+
+# def avg_3(a,b,c):
+#     avg= a+b+c/3
+#     print(avg)
+#     return(avg)
+# avg_3(2,4,8)
+
+
+# cities = ["Lucknow","Delhi","Hyderabad","Banaras"]
+# heroes  = ["Batman","Thor","Antman","Ironma"]
+
+# print(heroes[0], end='\n')
+# print(heroes[1], end='\n')
+# def print_len(list):
+#     print(len(list))
+
+
+# print_len(cities)
+# print_len(heroes)
+
+
+
+# light = "red"
+
+# if(light=="red"):
+#     print("stop")
+# elif(light=="green"):
+#     print(go)
+# elif(light=="Yellow"):
+#     print("slowdown")
+ 
+
+# marks = int(input())
+# if (marks>=90):
+#     print("A")
+# elif(75<=marks<=89):
+#     print("B")
+# elif(50<=marks<=74):
+#     print("C")
+# else:
+#     print("Fail")
+
+age = int(input())
+
+if age<0:
+    print("Invalid Age")
+elif age<=12:
+    print("Invalid Age")
+elif age<=18:
+    print("Ticket price is 100")
+else:
+    print("Ticket price is 150")
